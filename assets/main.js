c24376b49d5713fb0e46e1d1acd9f4a1a6169faf
ElianHernandez <elianhernandezc16@gmail.com>
2022-07-18 16:06:38 -0500

    Updates

--- assets/main.js
+++ assets/main.js
@@ -28,19 +28,20 @@ async function fetchData(urlApi){
         //en esta API , para acceder a los videos, se refiere a items, se hace un map para devolver un nuevo arreglo con el template por cada resultado
         let view = `
         ${videos.items.map(video=>`
-            <div class="group relative">
-            <div
-                class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
-                <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
-            </div>
-            <div class="mt-4 flex justify-between">
-                <h3 class="text-sm text-gray-700">
-                <span aria-hidden="true" class="absolute inset-0"></span>
-                ${video.snippet.title}
-                </h3>
-            </div>
-            </div>
-        `).slice(0,4).join('')}//para iterar solo 4 videos
+        <div class="group">
+        <div class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
+          <a target="_blank" href="https://youtube.com/video/${video.id.videoId}">
+            <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
+          </a>
+        </div>
+        <div class="mt-4 flex justify-between">
+          <h3 class="text-sm text-gray-700 dark:text-slate-400"">
+            <span aria-hidden="true" class="absolute inset-0"></span>
+            ${video.snippet.title}
+          </h3>
+        </div>
+      </div>
+        `).slice(0,4).join('')} <!--para iterar solo 4 videos-->
             
         `;
         content.innerHTML = view;
@@ -49,3 +50,4 @@ console.log(error);
     }
 })();
 	
+
