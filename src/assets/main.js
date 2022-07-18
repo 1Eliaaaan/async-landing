0af220ed16e34cc30085726fdeaeaff64ce1fd56
ElianHernandez <elianhernandezc16@gmail.com>
2022-07-18 16:56:07 -0500

    5

--- src/assets/main.js
+++ src/assets/main.js
@@ -41,7 +41,7 @@ async function fetchData(urlApi){
           </h3>
         </div>
       </div>
-        `).slice(0,4).join('')} <!--para iterar solo 4 videos-->
+        `).slice(0,8).join('')} <!--para iterar solo 4 videos-->
             
         `;
         content.innerHTML = view;
